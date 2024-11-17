0f6340941b29508212e53af11cf522b91d14bc76
rijwanrk <166917949+rijwanrk@users.noreply.github.com>
2024-11-17 20:35:55 +0530

    old version

--- next.config.js
+++ next.config.js
@@ -1,55 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "placehold.co",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "3000",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-//   async headers() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         headers: [
-//           { key: "Access-Control-Allow-Credentials", value: "true" },
-//           { key: "Access-Control-Allow-Origin", value: "*" },
-//           { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST" },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
-// next.config.js
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://explorella.onrender.com' 
-      : 'http://localhost:3000',
-  },
   images: {
     remotePatterns: [
       {
@@ -72,4 +23,23 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST" },
+        ],
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
+
+
+
+// next.config.js
+
