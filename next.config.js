0dd6161abe221ce87ff78ffcbd658288017690d0
rijwanrk <166917949+rijwanrk@users.noreply.github.com>
2024-11-17 22:59:09 +0530

    add

--- next.config.js
+++ next.config.js
@@ -16,16 +16,28 @@ const nextConfig = {
         pathname: "/**",
       },
       {
-        // protocol: "http",
-        // hostname: "localhost",
-        // port: "3000",
-        // pathname: "/**",
         protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace("http://", "").replace("https://", ""),
-        port: "",
+        hostname: "localhost",
+        port: "3000",
         pathname: "/**",
+        // protocol: "http",
+        // hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace("http://", "").replace("https://", ""),
+        // port: "",
+        // pathname: "/**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST" },
+        ],
+      },
+    ];
   },
   // async headers() {
   //   return [
@@ -34,33 +46,21 @@ const nextConfig = {
   //       headers: [
   //         { key: "Access-Control-Allow-Credentials", value: "true" },
   //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST,PUT,DELETE" },
+  //         { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+  //       ],
+  //     },
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         { key: "Content-Security-Policy", value: "default-src 'self'; img-src 'self' data: https:;" },
+  //         { key: "X-Frame-Options", value: "DENY" },
+  //         { key: "X-Content-Type-Options", value: "nosniff" },
+  //         { key: "Referrer-Policy", value: "no-referrer" },
   //       ],
   //     },
   //   ];
   // },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST,PUT,DELETE" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Content-Security-Policy", value: "default-src 'self'; img-src 'self' data: https:;" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "no-referrer" },
-        ],
-      },
-    ];
-  },
 
 };
 
